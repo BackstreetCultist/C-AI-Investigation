d1f644a9988ef81d2f3d69b03ec97a165626aa59
BackstreetCultist <charlie@101a.co.uk>
2020-01-11 16:14:05 +0000

    Expand skeleton

--- xorlearner.c
+++ xorlearner.c
@@ -3,14 +3,26 @@
 
 //x1 XOR x2 == (x1 AND NOT x2) OR (x2 AND NOT x1)
 int main(int argc, char **argv){
+	//Inputs
 	int x1[4] = {1,1,0,0};
 	int x2[4] = {1,0,1,0};
+	
+	//Mid-stage outputs
+	int z1[4] = {0,0,0,0};
+	int z2[4] = {0,0,0,0};
+	
 	int T[4] = {0,0,0,0}; //Training value
+	int *O; //Output
+	
+	//Get CLI
 	for (int i = 1; i<argc; i++){
 		T[i - 1] = argv[i][0] - '0';
 	}
+	
+	//Hidden Layer
 	printf("ENTERING HIDDEN LAYER -----------------\n");
-	//Hidden code 
+		
+
 	printf("ENTERING MAIN LAYER -----------------\n");
 	
 	
