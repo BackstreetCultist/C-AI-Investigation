14093908dde5ca79c4bf50c6acb00d72245557bc
BackstreetCultist <charlie@101a.co.uk>
2020-01-12 16:00:56 +0000

    Refactor learner to account for changes

--- learner.c
+++ learner.c
@@ -5,11 +5,11 @@ int main(int argc, char **argv){
 	int x1[4] = {1,1,0,0};
 	int x2[4] = {1,0,1,0};
 	int T[4] = {0,0,0,0}; //Training value
-	int *O; //Outputs
+	int O[4] = {0,0,0,0}; //Output
 	for (int i = 1; i<argc; i++){
 		T[i - 1] = argv[i][0] - '0';
 	}
-	O = perceptron(x1,x2,T);
+	perceptron(x1,x2,T,O);
 	
 	//Printing
 	printOutput(O);
