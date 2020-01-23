d88727110cf3e7dade9c85166bf0c88bc708a4ed
BackstreetCultist <charlie@101a.co.uk>
2020-01-23 11:19:46 +0000

    Refactor so that x, t and z are represented by 2D arrays

--- xorlearner.c
+++ xorlearner.c
@@ -9,16 +9,23 @@ int main(int argc, char **argv){
 	pthread_t nodes[2];
 	
 	//Inputs
-	int x1[4] = {1,1,0,0};
-	int x2[4] = {1,0,1,0};
+	int xs[2][4] = {
+		{1,1,0,0},
+		{1,0,1,0}
+	};
+
 	
 	//Mid-stage Targets
-	int t1[4] = {0,1,0,0};
-	int t2[4] = {0,0,1,0};
+	int ts[2][4] = {
+		{0,1,0,0},
+		{0,0,1,0}
+	};
 	
 	//Mid-stage outputs
-	int z1[4] = {0,0,0,0};
-	int z2[4] = {0,0,0,0};
+	int zs[2][4] = {
+		{0,0,0,0},
+		{0,0,0,0}
+	};
 
 	
 	int T[4] = {0,0,0,0}; //Training value
@@ -34,27 +41,10 @@ int main(int argc, char **argv){
 	
 	for(int i = 0; i < 2; i++){
 		printf("\nLEARNER: Creating thread %d\n", i);
-		
-		switch(i){
-			case 0:
-				if (pthread_create(&nodes[i], NULL, threadablePerceptron\
-					(x1, x2, t1, z1, (void *) &i), (void *) &i) == -1){
-					printf("Creating thread %d failed\n", i);
-					exit(-1);
-				}
-				break;
-				
-			case 1:
-				if (pthread_create(&nodes[i], NULL, threadablePerceptron\
-					(x1, x2, t2, z2, (void *) &i), (void *) &i) == -1){
-					printf("Creating thread %d failed\n", i);
-					exit(-1);
-				}
-				break;
-				
-			default:
-				printf("You shouldn't see this message\n");
-				break;
+		if (pthread_create(&nodes[i], NULL, threadablePerceptron\
+			(xs[0], xs[1], ts[i], zs[i], (void *) &i), (void *) &i) == -1){
+			printf("Creating thread %d failed\n", i);
+			exit(-1);
 		}
 
 		printf("LEARNER: Continuing with loop\n");
@@ -64,14 +54,14 @@ int main(int argc, char **argv){
 	pthread_join(nodes[0], NULL);
 	pthread_join(nodes[1], NULL);
 	
-	printOutput(z1);
-	printOutput(z2);
+	printOutput(zs[0]);
+	printOutput(zs[1]);
 	
 	
 	//Output Layer
 	printf("\nENTERING OUTPUT LAYER --------------------------------------\n");
 	
-	perceptron(z1, z2, T, O);
+	perceptron(zs[0], zs[1], T, O);
 	printOutput(O);
 	
 	return 0;
